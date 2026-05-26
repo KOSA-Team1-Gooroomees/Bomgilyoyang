027ed2456aecbdd9d69d0ccb446f9aadd73a966c
minho2618 <minho2618@naver.com>
2026-05-26 17:59:38 +0900

    feat#143 - User에 구현된 UserDetail 제거, UserDetail 구현한 User 래퍼 구현

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/entity/User.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/entity/User.java
@@ -20,13 +20,13 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
@@ -51,41 +51,6 @@ public class User implements UserDetails {
     private LocalDateTime createdAt;
 
     private LocalDateTime modifiedAt;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return status.equals(Status.ACTIVE);
-    }
 
     public void changeStatus(Status status) {
         this.status = status;
