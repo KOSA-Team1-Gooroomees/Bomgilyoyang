5ebd214aef42264da90745d3826944ffad9c39a5
minho2618 <minho2618@naver.com>
2026-05-27 17:53:47 +0900

    feat#169

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/controller/MyPageController.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/controller/MyPageController.java
@@ -2,13 +2,21 @@ package com.gooroomees.neulbomgil_backend.domain.auth.controller;
 
 import com.gooroomees.neulbomgil_backend.domain.auth.entity.CustomUserDetails;
 import com.gooroomees.neulbomgil_backend.domain.auth.entity.User;
+import com.gooroomees.neulbomgil_backend.domain.favorite.dto.response.FavoriteResponse;
+import com.gooroomees.neulbomgil_backend.domain.favorite.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-public class MyPageViewController {
+@RequiredArgsConstructor
+public class MyPageController {
+
+    private final FavoriteService favoriteService;
 
     @GetMapping("/mypage")
     public String mypageView(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
@@ -17,6 +25,10 @@ public class MyPageViewController {
         }
         User user = userDetails.getUser();
         model.addAttribute("user", user);
+
+        List<FavoriteResponse> favList = favoriteService.getUserFavoritesWithDetail(user.getUserId());
+        model.addAttribute("favList", favList);
+
         return "auth/mypage";
     }
 
