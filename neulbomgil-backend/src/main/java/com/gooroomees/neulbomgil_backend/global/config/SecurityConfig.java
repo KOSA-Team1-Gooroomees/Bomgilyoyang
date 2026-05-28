de08d79132565cbd16b775570eb4048a62b91841
minho2618 <minho2618@naver.com>
2026-05-28 10:30:06 +0900

    feat#169 - SecurityConfig 수정

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/global/config/SecurityConfig.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/global/config/SecurityConfig.java
@@ -23,26 +23,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> cors.configurationSource(request -> {
-                    var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(java.util.List.of("http://localhost:5173")); // 프론트 주소
-                    config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    config.setAllowedHeaders(java.util.List.of("*"));
-                    config.setAllowCredentials(true);
-                    return config;
-                }))
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers
                         .frameOptions(frame -> frame.disable())
                 )
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req
-                        //websocket연결
                         .requestMatchers("/**").permitAll()
-                        .requestMatchers("/api/favorites/**").authenticated()
                         .requestMatchers("/api/map/**").permitAll()
-                        .requestMatchers("/ws/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll() // websocket연결
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/email/**").permitAll()
                         .requestMatchers(
@@ -50,7 +38,6 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
-                        //.requestMatchers()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
@@ -73,36 +60,6 @@ public class SecurityConfig {
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 )
-                //조윤지
-//                .authorizeHttpRequests(req -> req
-//                        // 인증 없이 허용
-//                        .requestMatchers("/api/auth/**").permitAll()
-//                        .requestMatchers("/api/email/**").permitAll()
-//                        .requestMatchers("/api/map/**").permitAll()
-//                        .requestMatchers("/ws/**").permitAll()
-//                        .requestMatchers(
-//                                "/v3/api-docs/**",
-//                                "/swagger-ui/**",
-//                                "/swagger-ui.html"
-//                        ).permitAll()
-//
-//                        // 타임리프 정적 리소스 & 페이지
-//                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-//                        .requestMatchers("/", "/login", "/signup").permitAll()
-//
-//                        // 게시글 목록은 비로그인도 허용
-//                        .requestMatchers(HttpMethod.GET, "/boards").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/boards/**").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/boards").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/boards/sort/**").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/boards/search").permitAll()
-//
-//                        .requestMatchers(HttpMethod.GET, "/boards/sort/**").permitAll()   // 타임리프 정렬 페이지
-//                        .requestMatchers(HttpMethod.GET, "/boards/search").permitAll()    // 타임리프 검색 페이지
-//
-//                        // 나머지는 로그인 필요
-//                        .anyRequest().authenticated()
-//                )
                 .authenticationProvider(authenticationProvider)
                 .build();
     }
