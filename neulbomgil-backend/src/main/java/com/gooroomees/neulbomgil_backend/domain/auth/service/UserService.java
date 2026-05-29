a27876b6848cf11f3803cae7ba9bac6da4fd15c0
minho2618 <minho2618@naver.com>
2026-05-29 16:31:02 +0900

    fix#204 - 사용자 정보 수정 오류 해결

--- neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/service/UserService.java
+++ neulbomgil-backend/src/main/java/com/gooroomees/neulbomgil_backend/domain/auth/service/UserService.java
@@ -1,5 +1,6 @@
 package com.gooroomees.neulbomgil_backend.domain.auth.service;
 
+import com.gooroomees.neulbomgil_backend.domain.auth.dto.request.UserInfoChangeRequest;
 import com.gooroomees.neulbomgil_backend.domain.auth.entity.User;
 import com.gooroomees.neulbomgil_backend.domain.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,4 +21,27 @@ public class UserService {
     public User findByEmail(String email) {
         return userRepository.findByEmail(email).orElse(null);
     }
+
+    public User changeUserInfo(UserInfoChangeRequest request, User user) {
+        if (userRepository.findById(user.getUserId()).orElse(null) == null) {
+            return null;
+        }
+        log.info("Request Name: " + request.getName());
+        User newUser = User.builder()
+                .userId(user.getUserId())
+                .name(request.getName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .status(user.getStatus())
+                .provider(user.getProvider())
+                .providerId(user.getProviderId())
+                .createdAt(user.getCreatedAt())
+                .modifiedAt(user.getModifiedAt())
+                .build();
+        userRepository.save(newUser);
+
+        return newUser;
+    }
+
 }
